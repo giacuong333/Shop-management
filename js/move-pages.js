f784bcc9f21f2b3ebb7ccd98bbe9039d1c2346b9
Gia Cường <legiacuong789@gmail.com>
2023-05-22 23:42:53 +0700

    Done move-pages.js

--- js/move-pages.js
+++ js/move-pages.js
@@ -1,18 +1,24 @@
-// Move pages 
 const pageBefore = document.querySelector(".content-cart__pagination-before")
 const pageAfter = document.querySelector(".content-cart__pagination-after")
+const pageLinks = document.querySelectorAll(".content-cart__pagination-num__list-item a")
 
-// Count var to count page 
-var currentPageIndex = 1
-var totalPage = 4
-
+// Move forward
 const nextPage = () => {
-          if (currentPageIndex < totalPage)
-                    currentPageIndex++
-          var nextPageUrl = "./store_page" + currentPageIndex + ".html"
-          window.location.href = nextPageUrl
+          const currentPage = parseInt(document.querySelector(".content-cart__pagination-num__current").textContent)
+          if (currentPage < pageLinks.length + 1 && currentPage !== -1) {
+                    const nextPageLink = pageLinks[currentPage - 1].getAttribute("href")
+                    window.location.href = nextPageLink
+          }
 }
 
-pageAfter.addEventListener('click', () => {
-          nextPage()
-})
+// Move backward
+const previousPage = () => {
+          const currentPage = parseInt(document.querySelector(".content-cart__pagination-num__current").textContent)
+          if (currentPage > 1) {
+                    const previousPageLink = pageLinks[currentPage - 2].getAttribute("href")
+                    window.location.href = previousPageLink
+          }
+}
+
+pageAfter.addEventListener("click", nextPage)
+pageBefore.addEventListener("click", previousPage)
