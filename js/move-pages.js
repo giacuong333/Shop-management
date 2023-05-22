90de142df37b47715a2acdc7870cfced65cb04f9
Gia Cường <legiacuong789@gmail.com>
2023-05-22 20:35:59 +0700

    Fixed move pages

--- js/move-pages.js
+++ js/move-pages.js
@@ -1,23 +1,3 @@
-
-const pages = [
-          {
-                    id: 0,
-                    link: "../store_page1.html"
-          },
-          {
-                    id: 1,
-                    link: "../store_page2.html"
-          },
-          {
-                    id: 2,
-                    link: "../store_page3.html"
-          },
-          {
-                    id: 3,
-                    link: "../store_page4.html"
-          },
-]
-
 // Move pages 
 const pageBefore = document.querySelector(".content-cart__pagination-before")
 const pageAfter = document.querySelector(".content-cart__pagination-after")
@@ -27,12 +7,10 @@ var currentPageIndex = 1
 var totalPage = 4
 
 const nextPage = () => {
-          if (currentPageIndex < totalPage) {
+          if (currentPageIndex < totalPage)
                     currentPageIndex++
-                    var nextPageUrl = "./store_page" + currentPageIndex + ".html"
-                    console.log(nextPageUrl)
-                    window.location.href = nextPageUrl
-          }
+          var nextPageUrl = "./store_page" + currentPageIndex + ".html"
+          window.location.href = nextPageUrl
 }
 
 pageAfter.addEventListener('click', () => {
